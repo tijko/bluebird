62e64b4c7d470d60ba825b67d16165191011aa52
Tim <konick781@gmail.com>
2016-06-14 10:25:56 -0400

    Add in getsid call

--- test/alt_print.c
+++ test/alt_print.c
@@ -6,14 +6,20 @@ char *process = "Process";
 int main(int argc, char *argv[])
 {
     setlinebuf(stdout);
-    pid_t pid = getpid();
+    pid_t sid, pid = getpid();
+    int count = 0;
 
     while ( 1 ) {
 
         printf("%s <%d> is running!\n", process, pid);
         sleep(1);
+        count++;
 
+        if (count == 10)
+            sid = getsid(pid);
     }
         
+    printf("Session id: %d\n", sid);
+
     return 0;
 }
